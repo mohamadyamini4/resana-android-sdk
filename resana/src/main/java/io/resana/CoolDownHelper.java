0d6e0ca5096cc47a1a8063cc1a9950d11c6f8f83
Ehsan <ehsansouri23@gmail.com>
2018-12-27 16:20:56 +0330

    remove some methods

--- resana/src/main/java/io/resana/CoolDownHelper.java
+++ resana/src/main/java/io/resana/CoolDownHelper.java
@@ -2,12 +2,6 @@ package io.resana;
 
 import android.content.Context;
 
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_CHANCE;
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE;
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_INTERVAL;
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_TS;
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_TS;
-import static io.resana.ResanaPreferences.PREF_CTRL_NATIVE_COOL_DOWN_TTL;
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_CHANCE;
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE;
 import static io.resana.ResanaPreferences.PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE_INTERVAL;
@@ -48,15 +42,6 @@ class CoolDownHelper {
         return Math.random() <= chance;
     }
 
-    private static boolean isFirstNativeAd(Context context) {
-        final int interval = getInt(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_INTERVAL, -1);
-        final float first = getFloat(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE, -1);
-        final long lastTime = getLong(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_TS, -1);
-        if (interval < 0 || first < 0)
-            return false;
-        return System.currentTimeMillis() > lastTime + interval * 1000;
-    }
-
     static private void validateSplashCoolDownData(Context context) {
         final long ts = getLong(context, PREF_CTRL_SPLASH_COOL_DOWN_TS, -1);
         final long ttl = getInt(context, PREF_CTRL_SPLASH_COOL_DOWN_TTL, -1);
@@ -70,20 +55,5 @@ class CoolDownHelper {
         remove(context, PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE);
         remove(context, PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE_TS);
         remove(context, PREF_CTRL_SPLASH_COOL_DOWN_FIRST_CHANCE_INTERVAL);
-    }
-
-    static private void validateNativeAdCoolDownData(Context context) {
-        final long ts = getLong(context, PREF_CTRL_NATIVE_COOL_DOWN_TS, -1);
-        final long ttl = getInt(context, PREF_CTRL_NATIVE_COOL_DOWN_TTL, -1);
-        if (ts < 0 || ttl < 0)
-            return;
-        if (System.currentTimeMillis() < ts + ttl * 1000)
-            return;
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_CHANCE);
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_TS);
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_TTL);
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE);
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_TS);
-        remove(context, PREF_CTRL_NATIVE_COOL_DOWN_FIRST_CHANCE_INTERVAL);
     }
 }
