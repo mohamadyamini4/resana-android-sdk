b6ed2e7d8cb482ce04fe110024d9fc56bdd92920
Ehsan <ehsansouri23@gmail.com>
2018-12-29 11:46:51 +0330

    add some code beauty

--- resana/src/main/java/io/resana/ControlDto.java
+++ resana/src/main/java/io/resana/ControlDto.java
@@ -9,27 +9,21 @@ import java.io.Serializable;
 
 class ControlDto implements Parcelable, Serializable {
 
-    public static final Creator<ControlDto> CREATOR = new Creator<ControlDto>() {
-        @Override
-        public ControlDto createFromParcel(Parcel in) {
-            return new ControlDto(in);
-        }
-
-        @Override
-        public ControlDto[] newArray(int size) {
-            return new ControlDto[size];
-        }
-    };
     @SerializedName("ch0")
     float nativeChance = 1;
+
     @SerializedName("ch1")
     float splashChance = 1;
+
     @SerializedName("ttl")
     int controlsTTL = 1000;
+
     @SerializedName("rl")
     String resanaLabel = ResanaInternal.DEFAULT_RESANA_INFO_TEXT;
+
     @SerializedName("bz")
     String[] blockedZones;
+
     @SerializedName("ch2")
     int videoStickyChance = 1;
 
@@ -43,11 +37,6 @@ class ControlDto implements Parcelable, Serializable {
     }
 
     @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeFloat(nativeChance);
         dest.writeFloat(splashChance);
@@ -55,5 +44,22 @@ class ControlDto implements Parcelable, Serializable {
         dest.writeInt(controlsTTL);
         dest.writeString(resanaLabel);
         dest.writeStringArray(blockedZones);
+    }
+
+    public static final Creator<ControlDto> CREATOR = new Creator<ControlDto>() {
+        @Override
+        public ControlDto createFromParcel(Parcel in) {
+            return new ControlDto(in);
+        }
+
+        @Override
+        public ControlDto[] newArray(int size) {
+            return new ControlDto[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 }
