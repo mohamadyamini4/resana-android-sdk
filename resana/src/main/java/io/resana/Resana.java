9d3ab8d6628236e787f55fc0f9cd24171b790dbb
Ehsan <ehsansouri23@gmail.com>
2018-12-31 11:19:17 +0330

    make resana internal instance static

--- resana/src/main/java/io/resana/Resana.java
+++ resana/src/main/java/io/resana/Resana.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Resana {
-    ResanaInternal instance;
+    static ResanaInternal instance;
 
     private static Resana resana;
 
@@ -42,7 +42,7 @@ public class Resana {
             throw new IllegalArgumentException("ResanaConfig cannot be null");
         appContext = context;
         ResanaConfig.saveConfigs(appContext, resanaConfig);
-        ResanaInternal.getInstance(appContext);
+        instance = ResanaInternal.getInstance(appContext);
     }
 
     public static Resana getInstance() {
